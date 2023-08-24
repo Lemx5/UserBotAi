52a8b1d834fbc034b5aa96f1c6dd9e908fde0ebf
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-24 14:23:55 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# PaLM-UserBot
-A Telegram UserBot using Google PaLM API
+# PALM Telegram Bot 🤖
+#### A Telegram user bot powered by Google's PALM generativeai to instantly transform prompts into expansive text.
+
+### 🌟 Features
+Instant Response: Automatically listens to messages beginning with a . (dot).
+In-place Editing: Modifies the original message to display the newly generated content.
+Safe & Secure: All operations and data remain within your chosen hosting environment.
+
+### 🛠 Setup & Deployment
+#### Requirements
+- Python 3.6 or higher
+- Telegram API Key
+- Google Cloud API Key
+- Pyrogram
+
+#### Installation
+1. Clone the repository: `git clone https://github.com/irymee/PaLM-UserBot.git`
+2. Enter the directory: `cd PaLM-UserBot`
+3. Install the required dependencies: `pip3 install -r requirements.txt`
+4. Fill in the required values in `bot.py`
+5. Run the bot: `python3 bot.py`
+
+#### Deployment in Render
+Push the repository containing the Dockerfile and render.yml to GitHub or GitLab.
+Securely set your secrets (API_ID, API_HASH, SESSION_STRING, PALM_API_KEY) in the Render dashboard.
+Link your repository to Render. It will recognize the render.yml and initiate the deployment.
+
+### 💡 Usage
+1. Send a message beginning with a . (dot) to the bot.
+
+```javascript
+.Hello
+```
+2. Wait for the bot to respond with the generated text.
---
+++ bot.py
@@ -0,0 +1,81 @@
+import os
+import threading
+import google.generativeai as palm
+from pyrogram import Client, filters
+from flask import Flask
+
+# ------------------ Configuration ------------------
+
+# Environmental Variables
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+SESSION_STRING = os.environ.get("SESSION_STRING")
+PALM_API_KEY = os.environ.get("PALM_API_KEY")
+
+# Palm Client Configuration
+palm.configure(api_key=PALM_API_KEY)
+
+# Pyrogram Client Configuration
+userbot = Client(
+    session_string=SESSION_STRING,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
+
+# Flask App Initialization
+app = Flask(__name__)
+
+# ------------------ Palm Generator ------------------
+async def palmgen(text):
+    defaults = {
+        'model': 'models/text-bison-001',
+        'temperature': 0.7,
+        'candidate_count': 1,
+        'top_k': 40,
+        'top_p': 0.95,
+        'max_output_tokens': 1024,
+        'stop_sequences': [],
+        'safety_settings': [
+            {"category": "HARM_CATEGORY_DEROGATORY", "threshold": 4},
+            {"category": "HARM_CATEGORY_TOXICITY", "threshold": 4},
+            {"category": "HARM_CATEGORY_VIOLENCE", "threshold": 4},
+            {"category": "HARM_CATEGORY_SEXUAL", "threshold": 4},
+            {"category": "HARM_CATEGORY_MEDICAL", "threshold": 4},
+            {"category": "HARM_CATEGORY_DANGEROUS", "threshold": 4}
+        ]
+    }
+
+    response = palm.generate_text(**defaults, prompt=text)
+    return response.result
+
+# ------------------ Bot Commands ------------------
+
+@userbot.on_message(filters.text & ~filters.bot)
+async def generate_text(client, message):
+    # Check if the message starts with "."
+    if not message.text.startswith("."):
+        return
+
+    # Strip the leading "." and generate text based on the remaining message
+    prompt_text = message.text[1:]
+    generated_text = await palmgen(prompt_text)
+
+    # Edit the original message with the generated text
+    await message.edit_text(generated_text)
+
+# ------------------ Flask Routes ------------------
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+# ------------------ Main Execution ------------------
+
+def run_flask_app():
+    app.run(host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    # Use threading to run both Flask and Pyrogram concurrently
+    t = threading.Thread(target=run_flask_app)
+    t.start()
+    userbot.run()
